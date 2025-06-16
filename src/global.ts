042f537a31810570fac578d4985beb214003a53b
Jeya chandran s <jeyachandranjcs@gmail.com>
2025-06-16 16:00:50 +0530

    ok

--- src/global.ts
+++ src/global.ts
@@ -1,4 +1,1 @@
-// export const API:string="https://684bef05ed2578be881d0a1d.mockapi.io/Foodlist/Foods"
-
-
-export const API:string="https://684bef05ed2578be881d0a1d.mockapi.io/cartList/cart"
+export const API:string="https://684bef05ed2578be881d0a1d.mockapi.io/Foodlist/Foods"
